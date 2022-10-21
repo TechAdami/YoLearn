c70d50d81bef04be80b525ced71ed4def742249c
20191tiimi0283 <daniellydarochaevangelista@gmail.com>
2022-10-21 14:34:54 -0300

    Conclusão do design da tela de inicio

---
+++ app/src/main/java/com/techadami/yolearn/ProfessorDetalhesActivity.java
@@ -0,0 +1,14 @@
+package com.techadami.yolearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessorDetalhesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_detalhes);
+    }
+}
