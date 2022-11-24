aa4436bbf44af51f776375e457ef4331620ecca6
20191tiimi0283 <daniellydarochaevangelista@gmail.com>
2022-11-24 12:47:56 -0300

    Botões.

--- app/src/main/java/com/techadami/yolearn/CadastroActivity.java
+++ app/src/main/java/com/techadami/yolearn/CadastroActivity.java
@@ -9,6 +9,6 @@ public class CadastroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_login);
     }
 }
