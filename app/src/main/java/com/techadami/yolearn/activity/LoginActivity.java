e7c1f25841ed22e15a69c0fb50c7127f3ffe9619
TechAdami <projintegrador28@gmail.com>
2022-12-10 15:07:25 -0300

    Registro de usuário.

--- app/src/main/java/com/techadami/yolearn/activity/LoginActivity.java
+++ app/src/main/java/com/techadami/yolearn/activity/LoginActivity.java
@@ -2,7 +2,10 @@ package com.techadami.yolearn.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.techadami.yolearn.R;
 
@@ -11,6 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_login);
+
+        // Se o usuário ainda não tem login, então ele pode ir para a tela de cadastro e criar um
+        // novo usuário
+        Button btnRegisterNewUser = findViewById(R.id.btnRegister);
+        btnRegisterNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
